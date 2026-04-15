003aa6510c7abff96c50930c687f77f8b54d530a
onkarlohar777-droid <onkarlohar777@gmail.com>
2026-04-15 13:28:06 +0530

    Create ACT 2.py

---
+++ EXP 4/ACT 2.py
@@ -0,0 +1,6 @@
+# find average and topper marks
+marks = [85, 56, 78, 56, 90]
+average_marks = sum(marks) / len(marks)
+topper_marks = max(marks)
+print(f"Average marks: {average_marks:.2f}")
+print(f"Topper marks: {topper_marks}")
