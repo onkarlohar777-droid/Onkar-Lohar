36d758b19eb7be830f2ef5eaea408e4133d9078b
onkarlohar777-droid <onkarlohar777@gmail.com>
2026-04-15 13:32:19 +0530

    Create ACT 3.py

---
+++ EXP 4/ACT 3.py
@@ -0,0 +1,7 @@
+# Reverse a customer feedback message.
+feedback = "Quick delivery"
+
+# join() merges the characters back together
+reversed_feedback = "".join(reversed(feedback))
+
+print(reversed_feedback)
