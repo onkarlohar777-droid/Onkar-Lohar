d60d8081efbd8e6a9a6beddeb901a6e803ea9846
onkarlohar777-droid <onkarlohar777@gmail.com>
2026-04-15 12:07:26 +0530

    Create ACT 1.py

---
+++ EXP 2/ACT 1.py
@@ -0,0 +1,6 @@
+# Traffic police speed checking program
+speed = float(input("Enter vehicle speed (km/h): "))
+if speed > 60:
+  print("Overspeeding! You have to pay a fine.")
+else:
+  print("Speed is within the limit. Drive safely.")
