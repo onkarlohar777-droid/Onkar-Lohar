9c9088c6c7942b12e0864cc62e26364d1ce8943a
onkarlohar777-droid <onkarlohar777@gmail.com>
2026-04-17 18:28:20 +0530

    Create ACT 2.py

---
+++ EXP 5/ACT 2.py
@@ -0,0 +1,17 @@
+#Store roll numberin sets.find student present in both classes.
+
+#Roll numbers present in Class A and Class
+
+class a (101, 102, 105, 108, 110)
+
+class b (102, 103, 108, 112, 115)
+
+Find students present in both
+
+present in_both class_a.intersection(class_b)
+
+#Alternatively, using the & operator
+
+#present in both class a & class_b
+
+print("Students present in both (present_in_both)")
