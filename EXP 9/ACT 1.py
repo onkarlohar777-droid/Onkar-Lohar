8eae3f29d1aef5784c47f6f9284e2ac7f08bef78
onkarlohar777-droid <onkarlohar777@gmail.com>
2026-05-12 11:14:19 +0530

    Create ACT 1.py

---
+++ EXP 9/ACT 1.py
@@ -0,0 +1,29 @@
+import math
+
+try:
+
+print("Enter coordinates of first point:")
+
+x1= finat(input("x12"))
+
+y1= float(input("y):)
+
+print("Enter coordinates of second point:")
+
+x2= float(input("x2"))
+
+y2= float(input("y2"))
+
+#Oistance formula
+
+distance math.sqrt((x2x1)**2 (2-1)**2)
+
+print("Distance between the two points is", distance)
+
+except ValueError:
+
+print("Error Please enter valjd numeric values.")
+
+finally:
+
+print("Program executed successfully")
