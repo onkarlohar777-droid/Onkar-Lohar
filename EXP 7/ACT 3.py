f54043c9c8be5675fc4e8b13601d4e66aabb3275
onkarlohar777-droid <onkarlohar777@gmail.com>
2026-05-12 11:25:19 +0530

    Create ACT 3.py

---
+++ EXP 7/ACT 3.py
@@ -0,0 +1,32 @@
+class Student:
+
+<<>>
+
+def _init_(self, name, marks_list):
+  self.name = name
+ self.marks_list = marks _list
+
+def calculate_percentage(self): total sum(self.marks_list)
+percentage = total / len(self.marks_list)
+return percentage
+
+def calculate_grade(self):
+
+percentage = self.calculate percentage()
+
+if percentage >= 90: grade ="A"
+
+elif percentage >= 75: grade ="B"
+
+elif percentage >> 60: grade ="C"
+
+elif percentage >> 50: grade ="D"
+
+else:
+
+grade ="Fail"
+
+print("\nStudent Name: (self.name)")
+print(f"Marks: (self.marks_list)")
+print("Fercentage: (percentage:.2f)%)
+print("Grade (grade)"
